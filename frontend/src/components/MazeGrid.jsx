7c5d64703febd1f66c7de8dfb8511cbbab599487
APPLE <apple@APPLEs-MacBook-Pro.local>
2024-11-15 04:44:05 +0530

    updated

--- frontend/src/components/MazeGrid.jsx
+++ frontend/src/components/MazeGrid.jsx
@@ -3,9 +3,16 @@ import React from "react";
 import "../MazeGrid.css";
 
 
-function MazeGrid({ grid , theme}) {  // Accept grid as a prop
+function MazeGrid({ grid , theme}) {
+
+    const boardStyleBorder = {
+        border: (theme == "Light") ? 
+            "1px solid black" :
+            "1px solid white"
+    }
+
     return (
-        <div className = "maze-grid">
+        <div className = "maze-grid" style = {boardStyleBorder}>
             {grid.map((row, rowIndex) => (
                 <div key = {rowIndex} className = "maze-row">
                     {row.map((cell, cellIndex) => (
--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../SideBar.css";
 
@@ -9,6 +9,25 @@ function SideBar({ onThemeChange }) {
 		console.log("Radio button clicked:", event.target.value);
 		onThemeChange(event.target.value);
 	}
+
+	const [formData, setFormData] = useState(
+		{
+			algorithm: "",
+			option: ""
+		}
+	)
+
+	console.log(formData.algorithm," ", formData.option)
+
+	function handleChange(event) {
+		setFormData(prevFormaData => {
+			return {
+				...prevFormaData,
+				[event.target.name]: event.target.value
+			}
+		})
+	}
+
 
     return (
         <div className = "sideBar">
@@ -38,7 +57,12 @@ function SideBar({ onThemeChange }) {
 			<div>
 				<form action = "">
 					<label htmlFor = "">algorithms:</label>
-					<select name = "" id = "algorithms">
+					<select 
+						name = "algorithm" 
+						id = "algorithms" 
+						value = {formData.algorithm} 
+						onChange = {handleChange}
+					>
 						<option value = "">--Please choose an option--</option>
 						<option value = "BFS">BFS</option>
 						<option value = "DFS">DFS</option>
@@ -47,11 +71,16 @@ function SideBar({ onThemeChange }) {
 					<br />
 
 					<label htmlFor = "">maze options:</label>
-					<select name = "" id = "mazes">
+					<select 
+						name = "option" 
+						id = "mazes" 
+						value = {formData.option} 
+						onChange = {handleChange}
+					>
 						<option value = "">--Please choose an option--</option>
 						<option value = "maze 1">maze 1</option>
 						<option value = "maze 2">maze 2</option>
-						<option value = "maze 2">auto-generation</option>
+						<option value = "auto">auto-generation</option>
 					</select>
 					
 					<br />
